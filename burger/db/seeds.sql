9b638a63062dddb090f190476d36c7e359fc0832
Marylee Pabst <marylee@uconn.edu>
2020-10-15 02:27:44 -0700

    db files created

---
+++ burger/db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (id, burger_name, devoured) VALUES (1, 'Bacon Cheeseburger', True);
+INSERT INTO burgers (id, burger_name, devoured) VALUES (2, 'Turkey Lurkey', True);
+INSERT INTO burgers (id, burger_name, devoured) VALUES (3, 'Romanes of the Day Burger', True);
